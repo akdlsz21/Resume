e0c2de8cb04d8a9a40d03f345e74d0535c2bb565
akdlsz21 <akdlsz21@gmail.com>
2022-12-13 11:18:49 +0900

    intro updated

--- components/MidSection/MidIntro.tsx
+++ components/MidSection/MidIntro.tsx
@@ -4,14 +4,15 @@ import HeaderText from '../texts/HeaderText';
 const MidIntro = () => {
 	return (
 		<div className=" flex flex-row items-center justify-center w-full mt-12 ">
-			<div className=" px-20 w-4/5  bg-gray-100 h-56 flex flex-row align-middle justify-between">
+			<div className=" px-20 w-4/5  bg-gray-100 pb-4 flex flex-row align-middle justify-between">
 				<div className=" h-full w-full mt-4">
 					<HeaderText text="Introduction" />
 					<p className="text-gray-500 text-base whitespace-pre-line mt-4">
-						{`21년 4월을 시작으로 개발을 독학하였습니다.
-						사용자 경험을 중심으로 생각을 하며 개발을 진행하고 있습니다.
-						주로 협력을 통한 프로젝트를 개발하였습니다.
-						최근에는 프론트 팀의 리더로서 프로젝트를 진행하였습니다.
+						{`여러 프로젝트를 해왔던 저는, 팀 내에서 실력이 제일 안 좋았던 개발자였던적도 있었습니다. 그리고 마지막 프로젝트에서는 경험이 제일 풍부한 경험자로 프로젝트를 완성하였습니다. 기획 단계에서부터 디버깅까지, 전 프로세스를 다 책임지면서 각 팀원에게 작업량을 배분해주기도 하였습니다.
+
+그리고 그동안 여러 팀프로젝트와 솔로 프로젝트를 해오면서 깨닫게 된것은, 무엇을 해야 할지는 엔드유저의 경험을 위주로 생각하는것. 그리고 어떻게 할지는 팀원간의 철저한 토의와 논리적 사고로 철저하게 대비해야 한다는것을 깨달았습니다.
+
+앞으로 같이 일하게 될 팀을 위해서 빠르게 성장할 수 있는 개발자가 되겠습니다. 그리고 문제해결을 위해 깊게 고민하고 철저하게 솔루션을 탐색할 수 있는 개발자가 되겠습니다.
 						`}
 					</p>
 				</div>
